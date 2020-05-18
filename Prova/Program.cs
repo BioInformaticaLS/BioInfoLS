e2f1b7d7ec0ed3e3135d59e5965096a72e49d80d
luca lombardo <lucalombardo03@gmail.com>
2020-05-18 10:56:23 +0200

    Add project files.

---
+++ Prova/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Prova
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string stringa = ClassBioInfo.BioInformatica.LetturaStringha2();
+            //Console.Write(stringa);
+            long lunghezzasensibile = ClassBioInfo.BioInformatica.LunghezzaStringa1();
+            Console.WriteLine(lunghezzasensibile);
+            long lunghezzaresistente = ClassBioInfo.BioInformatica.LunghezzaStringa2();
+            Console.WriteLine(lunghezzaresistente);
+            long differenza = ClassBioInfo.BioInformatica.Prima_differenza();
+            Console.WriteLine(differenza);
+            string parte_uguale = ClassBioInfo.BioInformatica.Parte_Uguale();
+            Console.WriteLine(parte_uguale);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TestBioInfo/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestBioInfo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestBioInfo")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5c435ff5-2578-4bca-b903-e3e00eb32556")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
